9fc97c2136d9327faa22afce615675c6c69f0816
eunchong2lee <tlfqjrjs1234@naver.com>
2023-01-09 22:53:17 +0900

    [UPDATE] REGISTER

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -26,22 +26,75 @@ import axios from "axios";
 
 import "../../components/ProductionLayout/_healthItem.scss";
 
-const ACCCOUNT_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-
-const checkaccount = /^[a-z0-9]{4,16}$/;
-
-const regexaccount = (value: string) => {
-  return checkaccount.test(value);
-};
+//
+const emailRegex =
+  /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
 const RegisterIndividual = () => {
+  // password , account 유효성 검사
+
+  //// data 검사
+  const [account, setAccount] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  //// 에러시 메세지
+  const [accountMessage, setAccountMessage] = useState<string>("");
+  const [passwordMessage, setPasswordMessage] = useState<string>("");
+
+  //// 유효성 검사
+  const [isAccount, setIsAccount] = useState<boolean>(false);
+  const [isPassword, setIsPassword] = useState<boolean>(false);
+
+  //// modal 창
+  const [modalMessage, setModalMessage] = useState<string[]>(["", ""]);
+  const [isModal, setIsModal] = useState(false);
+  const Istoggle = () => setIsModal(!isModal);
+
+  //// onChange 함수
+  const onChangeAccount = (e: { target: { value: any; name: any } }) => {
+    const { name, value } = e.target;
+    const accountRegex = /^[a-z0-9]{4,16}$/;
+    const accountCurrent = e.target.value;
+    setAccount(accountCurrent);
+    setData({
+      ...data, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
+
+    if (!accountRegex.test(accountCurrent)) {
+      setAccountMessage("아이디 형식이 틀렸어요! 다시 확인해주세요 ㅜ ㅜ");
+      setIsAccount(false);
+    } else {
+      setAccountMessage(
+        "올바른 아이디 형식이에요 : ) 중복 아이디를 확인해주세요!"
+      );
+      setIsAccount(true);
+    }
+  };
+
+  const onChangePassword = (e: { target: { value: any; name: any } }) => {
+    const { name, value } = e.target;
+    const passwordRegex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
+    setData({
+      ...data, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage(
+        "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
+      );
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("안전한 비밀번호에요 : )");
+      setIsPassword(true);
+    }
+  };
+
   const dispatch = useDispatch();
-
-  const [message, setMessage] = useState("");
-
-  const [checkmodal, setCheckmodal] = useState(false);
-  const Checkmodal = () => setCheckmodal(!checkmodal);
 
   const { user, registrationError } = useSelector((state: any) => ({
     user: state.register.user,
@@ -62,41 +115,37 @@ const RegisterIndividual = () => {
       ...data, // 기존의 input 객체를 복사한 뒤
       [name]: value, // name 키를 가진 값을 value 로 설정
     });
-    console.log(data);
   };
 
   const checkAccount = async () => {
-    console.log("check요청");
-    const checkresponse = regexaccount(data.account);
-    if (checkresponse == false) {
-      setMessage("영소문자 숫자만 사용가능하고 4글자이상 16글자 이하입니다.");
-      Checkmodal();
-      return;
-    }
     await axios
       .post(`http://localhost:3000/auth/checkaccount`, data)
       .then(response => {
         console.log(response.data);
         if (response.data == 200) {
-          console.log("사용 가능합니다.");
+          setAccountMessage("사용 가능한 아이디 형식이에요 : )");
         } else {
-          setMessage("사용 불가능한 아이디입니다.");
-          Checkmodal();
+          setAccountMessage("이미 사용중인 아이디입니다.");
         }
       });
   };
 
   const singUP = async () => {
-    console.log("가입요청");
-    console.log(data);
-    await axios
-      .post(`http://localhost:3000/auth/signup`, data)
-      .then(response => {
-        console.log(response);
-        if (response.status === 200) {
-          window.location.href = `/login`;
-        }
-      });
+    if (isAccount && isPassword) {
+      await axios
+        .post(`http://localhost:3000/auth/signup`, data)
+        .then(response => {
+          console.log(response);
+          setModalMessage(["성공", "회원가입에 성공했습니다."]);
+          Istoggle();
+          // if (response.status === 200) {
+          //   window.location.href = `/login`;
+          // }
+        });
+    } else {
+      setModalMessage(["실패", "아이디 또는 비밀번호를 확인하세요"]);
+      Istoggle();
+    }
   };
 
   useEffect(() => {
@@ -150,7 +199,6 @@ const RegisterIndividual = () => {
                         name="managerName"
                         label="담당자 이름"
                         type="text"
-                        required
                         placeholder="담당자 이름 입력"
                         onChange={onChange}
                       />
@@ -164,7 +212,6 @@ const RegisterIndividual = () => {
                           className="form-control"
                           placeholder="부서를 입력해주세요"
                           type="text"
-                          required
                           onChange={onChange}
                         />
                       </div>
@@ -177,7 +224,6 @@ const RegisterIndividual = () => {
                           className="form-control"
                           placeholder="직급을 입력해주세요"
                           type="text"
-                          required
                           onChange={onChange}
                         />
                       </div>
@@ -190,7 +236,6 @@ const RegisterIndividual = () => {
                         className="form-control"
                         placeholder="이메일 주소를 입력해주세요"
                         type="text"
-                        required
                         onChange={onChange}
                       />
                     </div>
@@ -199,7 +244,6 @@ const RegisterIndividual = () => {
                         name="phone"
                         label="휴대폰번호"
                         type="text"
-                        required
                         placeholder="-없이 숫자만 입력"
                         onChange={onChange}
                       />
@@ -212,9 +256,8 @@ const RegisterIndividual = () => {
                             name="account"
                             label="아이디"
                             type="text"
-                            required
                             placeholder="영어 소문자 및 숫자로만, 4~16자리"
-                            onChange={onChange}
+                            onChange={onChangeAccount}
                           />
                         </Col>
                         <Col>
@@ -229,28 +272,31 @@ const RegisterIndividual = () => {
                               체크
                             </button>
                           </div>
-                          <Modal isOpen={checkmodal} toggle={Checkmodal}>
-                            {/* <Modal isOpen={modal} toggle={toggle} {...args}> */}
-                            <ModalHeader toggle={Checkmodal}>에러</ModalHeader>
-                            <ModalBody>{message}</ModalBody>
-                            <ModalFooter>
-                              <Button color="primary" onClick={Checkmodal}>
-                                확인
-                              </Button>
-                            </ModalFooter>
-                          </Modal>
                         </Col>
                       </Row>
+                      <div>
+                        {isAccount ? (
+                          <div>{accountMessage}</div>
+                        ) : (
+                          <div>{accountMessage}</div>
+                        )}
+                      </div>
                     </div>
                     <div className="form-floating form-floating-custom mb-3">
                       <AvField
                         name="password"
                         label="비밀번호"
                         type="password"
-                        required
                         placeholder="영어, 숫자, 특수문자 조합 8~16자리"
-                        onChange={onChange}
+                        onChange={onChangePassword}
                       />
+                      <div>
+                        {isPassword ? (
+                          <div>{passwordMessage}</div>
+                        ) : (
+                          <div>{passwordMessage}</div>
+                        )}
+                      </div>
                     </div>
 
                     <div className="text-start">
@@ -273,6 +319,29 @@ const RegisterIndividual = () => {
                         Register
                       </button>
                     </div>
+                    {isModal ? (
+                      <Modal isOpen={isModal} toggle={Istoggle}>
+                        {/* <Modal isOpen={modal} toggle={toggle} {...args}> */}
+                        <ModalHeader toggle={Istoggle}>
+                          {modalMessage[0]}
+                        </ModalHeader>
+                        <ModalBody>{modalMessage[1]}</ModalBody>
+                        <ModalFooter>
+                          <Button
+                            color="primary"
+                            type="button"
+                            onClick={() => {
+                              Istoggle();
+                              if (isAccount && isPassword) {
+                                window.location.href = `/login`;
+                              }
+                            }}
+                          >
+                            확인
+                          </Button>{" "}
+                        </ModalFooter>
+                      </Modal>
+                    ) : null}
                   </AvForm>
 
                   <div className="mt-5 text-center">
